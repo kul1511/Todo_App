5e79ce8043e93fc4fa45829d57f822ba33843259
Kuldeep Singh <rajputsinghkuldeep@gmail.com>
2024-09-02 18:34:52 +0530

    .

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
 const requestHandler = require('./routes/todos')
